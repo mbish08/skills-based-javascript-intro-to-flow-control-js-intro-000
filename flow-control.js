71f6fe98a229f4674be122413a3baae757570ce7
remarkable-assembler-5835 <mdrew8@gmail.com>
2020-08-09 19:43:58 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -10,4 +10,8 @@ function switchAge(age) {switch(age) {
   case 13:return 'You are a teenager';
   case 14:return "You are a teenager";
   case 15:return 'You are a teenager';
-  case 16:console.log("You are a teenager"); break; case 17:console.log("You are a teenager"); break; case 18:console.log("You are a teenager"); break; case 19:console.log("You are a teenager"); break; case(age >19):console.log('You have an age'); break; case (age <13):console.log('You have an age'); break; default:console.log('You have an age today');}}
+  case 16:return 'You are a teenager';
+  case 17:return 'You are a teenager';
+  case 18:return 'You are a teenager';
+  case 19:return 'You are a teenager';
+  default:return'You have an age today';}}
