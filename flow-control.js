8395c6865a5888fe7ce8eba6996c4364dc6903dd
remarkable-assembler-5835 <mdrew8@gmail.com>
2020-08-09 19:34:06 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -7,6 +7,3 @@ function ageChecker(age) {let result; if (age >= 13 && age <=19) {result = "You 
 function ternaryTeenager(age) {return age >= 13 && age <= 19 ? 'You are a teenager' : 'You are not a teenager';}
 
 function switchAge(age) {switch(age) {case 13:console.log("You are a teenager"); break; case 14:console.log("You are a teenager"); break; case 15:console.log("You are a teenager"); break; case 16:console.log("You are a teenager"); break; case 17:console.log("You are a teenager"); break; case 18:console.log("You are a teenager"); break; case 19:console.log("You are a teenager"); break; case(age >19):console.log('You have an age'); break; case (age <13):console.log('You have an age'); break; default:console.log('You have an age today');}}
-=======
-function switchAge(age) {switch(age) {case 13:console.log("You are a teenager"); break; case 14:console.log("You are a teenager"); break; case 15:console.log("You are a teenager"); break; case 16:console.log("You are a teenager"); break; case 17:console.log("You are a teenager"); break; case 18:console.log("You are a teenager"); break; case 19:console.log("You are a teenager"); break; case(age >19):console.log('You have an age'); break; case (age <13):console.log('You have an age'); break; default:console.log('You have an age');}}
->>>>>>> b972e0ec08d4110ca3162a9096f992ed5acc85bb
